f568797237d79565768fa57fe36290f694ee9293
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2023-03-11 13:20:29 +0530

    fix: warnings in LocalNews Class Definition

--- LocalNews.cs
+++ LocalNews.cs
@@ -2,9 +2,9 @@ namespace ASP_React_SPA;
 
 public class LocalNews
 {
-	public string Title { get; set; }
-	public string Body { get; set; }
-	public string Url { get; set; }
-	public string? ImageUrl { get; set; }
-	public DateTime PublishedAt { get; set; }
+    public string? Title { get; set; }
+    public string? Body { get; set; }
+    public string? Url { get; set; }
+    public string? ImageUrl { get; set; }
+    public DateTime PublishedAt { get; set; }
 }
